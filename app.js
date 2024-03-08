854126a6cd4cf0928118a020ed60bf73830dbc7b
Ninive97 <138055308+Ninive97@users.noreply.github.com>
2024-03-07 18:13:37 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//Invocamos a express
+const express = require('express');
+const app = express();
+
+//Plantilla ejs
+app.set('view engine', 'ejs');
+
+// seteamos urlencoded para capturar los datos del formulario 
+app.use(express.urlencoded({extended:false}));
+app.use(express(JSON));
+
+
+app.use('/', require('./router'));
+
+app.listen(5000, ()=>{
+    console.log('SERVER corriendo en http://localhost:5000');
+});
