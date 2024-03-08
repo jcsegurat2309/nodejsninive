02e5c41ba7b00e5e6d8d0dd55119502b4c7097f9
Ninive97 <138055308+Ninive97@users.noreply.github.com>
2024-03-07 18:13:52 -0600

    Create db.js

---
+++ database/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password: '',
+    database: 'crud_node'
+});
+
+conexion.connect((error)=>{
+    if(error){
+        console.error('El error de conexion es: ' ,error);
+        return;
+    }
+    console.log('Conectado a la BD');
+})
+
+module.exports=conexion;
